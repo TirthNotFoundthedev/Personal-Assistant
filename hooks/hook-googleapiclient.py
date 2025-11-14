27471c660ea74fd476e6c333fcf29c4ca335960e
Tirth <75626066+TirthNotFoundthedev@users.noreply.github.com>
2025-11-14 23:44:14 +0530

    fix: Refine PyInstaller hook for Google API client libraries

--- hooks/hook-googleapiclient.py
+++ hooks/hook-googleapiclient.py
@@ -1,6 +1,7 @@
 from PyInstaller.utils.hooks import collect_submodules
 
-hiddenimports = collect_submodules('googleapiclient') + \
-                collect_submodules('google.generativeai') + \
-                collect_submodules('google.api_core') + \
-                collect_submodules('google')
+hiddenimports = collect_submodules('google.generativeai') + \
+                collect_submodules('google.auth') + \
+                collect_submodules('google_auth_oauthlib') + \
+                collect_submodules('googleapiclient') + \
+                collect_submodules('google.api_core')
